a47e9ae7c3f415713e028d3f6815db7f1b1caa0d
Sidney Rodrigues <sidneyluiz@byui.com>
2018-03-15 02:03:03 -0600

    js

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -1,19 +1,21 @@
 /* *************************************
- *  Weather Site JavaScript Functions
- ************************************* */
+*  Weather Site JavaScript Functions
+************************************* */
 
 /*console.log('My javascript is being read.'); */
 
 // Variables for Function Use
 
-const temp = 31;
-const speed = 5;
-buildWC(speed, temp);
-const direction = "WSW";
-windDial(direction);
-const WEATHER = "mist";
-let curWeatherImage = getCondition(WEATHER);
-changeSummaryImage(curWeatherImage);
+//const temp = 31;
+//const speed = 5;
+//buildWC(speed, temp);
+//const direction = "WSW";
+//windDial(direction);
+//const WEATHER = "mist";
+//changeSummaryImage(curWeatherImage);
+
+
+
 
 /* This function calculates a wind chill temperature */
 function buildWC(speed, temp) {
@@ -45,79 +47,92 @@ function windDial(direction) {
     console.log(direction);
 
     /* ***********************************************
-            EXTRA - I created one extra line
-            to change the direction label
-    ************************************************ */
-    const labelDirection =document.getElementById("directionLabel");
+           EXTRA - I created one extra line
+           to change the direction label
+************************************************ */
+    const labelDirection = document.getElementById("directionLabel");
 
     // Determine the dial class
     switch (direction) {
         case "North":
         case "N":
             dial.setAttribute("class", "n"); //"n" is the CSS rule selector
-/**EXTRA**/ labelDirection.innerHTML = "N";
+            /**EXTRA**/ labelDirection.innerHTML = "N";
             break;
         case "NE":
         case "NNE":
         case "ENE":
             dial.setAttribute("class", "ne");
-/**EXTRA**/ labelDirection.innerHTML = "NE";
+            /**EXTRA**/ labelDirection.innerHTML = "NE";
             break;
         case "NW":
         case "NNW":
         case "WNW":
             dial.setAttribute("class", "nw");
-/**EXTRA**/ labelDirection.innerHTML = "NW";
+            /**EXTRA**/ labelDirection.innerHTML = "NW";
             break;
         case "South":
         case "S":
             dial.setAttribute("class", "s");
-/**EXTRA**/ labelDirection.innerHTML = "S";
+            /**EXTRA**/ labelDirection.innerHTML = "S";
             break;
         case "SE":
         case "SSE":
         case "ESE":
             dial.setAttribute("class", "se");
-/**EXTRA**/ labelDirection.innerHTML = "SE";
+            /**EXTRA**/ labelDirection.innerHTML = "SE";
             break;
         case "SW":
         case "SSW":
         case "WSW":
             dial.setAttribute("class", "sw");
-/**EXTRA**/ labelDirection.innerHTML = "SW";
+            /**EXTRA**/ labelDirection.innerHTML = "SW";
             break;
         case "East":
         case "E":
             dial.setAttribute("class", "e");
-/**EXTRA**/ labelDirection.innerHTML = "E";
+            /**EXTRA**/ labelDirection.innerHTML = "E";
             break;
         case "West":
         case "W":
             dial.setAttribute("class", "w");
-/**EXTRA**/ labelDirection.innerHTML = "W";
+            /**EXTRA**/ labelDirection.innerHTML = "W";
             break;
     }
 
 }
 
 /* **********************************
- JavaScript Introduction Assessment
+JavaScript Introduction Assessment
 *********************************** */
 
 //Write the getCondition function.
-function getCondition(WEATHER) {
+function getCondition(weather) {
 
-    if (WEATHER.includes('blue sky') || WEATHER.includes('sunny') || WEATHER.includes('just sun') || WEATHER.includes('clear sky')) {
+
+
+    if (weather.includes('blue sky') || weather.includes('sunny') || weather.includes('just sun') || weather.includes('clear sky')) {
         return "clear";
-    } else if (WEATHER.includes("partly cloudy") || WEATHER.includes("clouds on the sky") || WEATHER.includes('clouds')) {
+    } else if (weather.includes("partly cloudy") || weather.includes("clouds on the sky") || weather.includes('clouds')) {
         return "clouds";
-    } else if (WEATHER.includes('mist') || WEATHER.includes('foggy') || WEATHER.includes('smog') || WEATHER.includes('haze')) {
-        return "fog";
-    } else if (WEATHER.includes('rainning') || WEATHER.includes('wet wheather') || WEATHER.includes('flood') || WEATHER.includes('precipitation')) {
+    } else if (weather.includes('mist') || weather.includes('foggy') || weather.includes('smog') || weather.includes('haze')) {
+        return weather = "fog";
+    } else if (weather.includes('rain') || weather.includes('wet wheather') || weather.includes('flood') || weather.includes('precipitation')) {
         return "rain";
-    } else(WEATHER.includes('snowstorm') || WEATHER.includes('snowing') || WEATHER.includes('blizzard') || WEATHER.includes('snowfall'))
+    } else(weather.includes('Snow') || weather.includes('Heavy Snow') || weather.includes('blizzard') || weather.includes('snowfall') || weather.includes('Light Snow'))
     return "snow";
 }
+
+//variable to grab weather
+/*let weather = "mist";
+console.log(weather);
+getCondition(weather);
+getCondition(weather);
+console.log(weather);
+let curWeatherImage = getCondition(weather);
+console.log(curWeatherImage);
+/*changeSummaryImage(curWeatherImage);
+*/
 
 //Write the changeSummaryImage() function.
 function changeSummaryImage(curWeatherImage) {
@@ -149,3 +164,93 @@ function changeSummaryImage(curWeatherImage) {
 
     }
 }
+
+
+// Get Data from API
+function getData(LOCALE) {
+    const WU_API_KEY = '75576a32a1a0330a';
+    const URL = "https://api.wunderground.com/api/" + WU_API_KEY + "/almanac/conditions/forecast/hourly/q/" + LOCALE + ".json";
+    fetch(URL)
+        .then(response => response.json())
+        .then(function (data) {
+        console.log('Json object from getData function:');
+        console.log(data);
+        displayData(data);
+
+    })
+        .catch(error => console.log('There was an error: ', error))
+} // end getData function
+
+//display JSON data
+function displayData(data){
+    //weather condition
+    let weather = data.current_observation.weather;
+    console.log(weather);
+    //change background based on the weather
+    let curWeatherImage = getCondition(weather);
+    changeSummaryImage(curWeatherImage);
+    //Current temperature
+    let temp = data.current_observation.temp_f;
+    console.log(temp);
+    //Display current temperature
+    let curTemp = document.getElementById('tempRightNow');
+    let ct = temp+'&deg;F';
+    curTemp.innerHTML = ct;
+    //Wind Speed
+    let speed = data.current_observation.wind_mph;
+    console.log(speed);
+    //Display Speed
+    let windSpeed = document.getElementById('speed');
+    let spd = speed + ' mph';
+    windSpeed.innerHTML = spd;
+    //Feels Like Function
+    buildWC(speed, temp);
+    //Wind direction
+    let direction = data.current_observation.wind_dir;
+    console.log(direction);
+    windDial(direction);
+    //High Temperature
+    let high = data.almanac.temp_high.normal.F;
+    console.log(high);
+    //Low Temperature
+    let low = data.almanac.temp_low.normal.F;
+    console.log(low);
+    //Display High Temp
+    let hot = document.getElementById('hot');
+    hot.innerHTML = high+'&deg;F';
+    //Display Low Temp
+    let cold = document.getElementById('cold');
+    cold.innerHTML = low+'&deg;F';
+    //Gust
+    let gust = data.current_observation.wind_gust_mph;
+    console.log(gust);
+    //Display Gust
+    let mphGust = document.getElementById('gust');
+    mphGust.innerHTML = gust+' mph';
+    //Zip Code
+    let zip = data.current_observation.display_location.zip;
+    console.log(zip);
+    //Display Zip
+    let idZip = document.getElementById('zip');
+    idZip.innerHTML = zip;
+    //Elevation
+    let elevation = data.current_observation.display_location.elevation;
+    console.log(elevation);
+    //Display Elevation
+    let idElevation = document.getElementById('elevation');
+    idElevation.innerHTML = elevation + ' ft.';
+    //Location
+    let lat = data.current_observation.display_location.latitude;
+    console.log(lat);
+    let long = data.current_observation.display_location.longitude;
+    console.log(long);
+    //Display location
+    let loc = document.getElementById('location');
+    loc.innerHTML = lat + ' , ' + long;
+    //Hourly Forecast
+    let am = data.hourly_forecast;
+    console.log(am);
+
+}
+
+
---
+++ weather/js/geo_loc.js
@@ -0,0 +1,48 @@
+//this functions will work together to get weather information for the current location and populate a web page with the data.
+'use strict';
+
+// Call the function to get our location
+getGeoLocation();
+
+
+
+
+
+// Gets longitude and latitude of current location
+function getGeoLocation() {
+    const STATUS = document.getElementById('status');
+    STATUS.innerHTML = 'Getting Location...';
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            const LAT = position.coords.latitude;
+            const LONG = position.coords.longitude;
+
+            // Combine the values
+            const LOCALE = LAT + "," + LONG;
+            console.log(`Lat and Long are: ${LOCALE}.`);
+
+            // Call getData function, send locale
+            getData(LOCALE);
+
+        })
+    } else {
+        STATUS.innerHTML = "Your browser doesn't support Geolocation or it is not enabled!";
+    } // end else
+
+} // end getGeoLocation
+
+
+// Get Data from API
+//function getData(LOCALE) {
+//    const WU_API_KEY = '75576a32a1a0330a';
+//    const URL = "https://api.wunderground.com/api/" + WU_API_KEY + "/conditions/q/" + LOCALE + ".json";
+//    fetch(URL)
+//        .then(response => response.json())
+//        .then(function (data) {
+//        console.log('Json object from getData function:');
+//        console.log(data);
+//        displayData(data);
+//    })
+//        .catch(error => console.log('There was an error: ', error))
+//} // end getData function
